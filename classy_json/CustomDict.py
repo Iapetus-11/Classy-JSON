ea1e375861a1e011cea7e7020e2ac51aeab5f3c4
Iapetus11 <olimdahacker@gmail.com>
2020-08-19 18:18:58 -0400

    fix weird indentation

--- classy_json/CustomDict.py
+++ classy_json/CustomDict.py
@@ -2,10 +2,10 @@ class CustomDict(dict):
     def __init__(self, _dict):
         dict.__init__(self, _dict)
 
-    for key in list(_dict):
-        if isinstance(_dict[key], list):
-            self.__dict__[key] = CustomList(_dict[key])
-        elif isinstance(_dict[key], dict):
-            self.__dict__[key] = CustomDict(_dict[key])
-        else:
-            self.__dict__[key] = _dict[key]
+        for key in list(_dict):
+            if isinstance(_dict[key], list):
+                self.__dict__[key] = CustomList(_dict[key])
+            elif isinstance(_dict[key], dict):
+                self.__dict__[key] = CustomDict(_dict[key])
+            else:
+                self.__dict__[key] = _dict[key]
