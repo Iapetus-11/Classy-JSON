82a5ecba1e553b9c9b9abc73f7cb86a43181cdac
Iapetus11 <olimdahacker@gmail.com>
2020-08-19 19:11:40 -0400

    remove unneeded list to attributes

--- classy_json/CustomList.py
+++ classy_json/CustomList.py
@@ -1,11 +1,9 @@
 class CustomList(list):
     def __init__(self, _list):
-        list.__init__(self, _list)
-
         for i, val in enumerate(_list):
             if isinstance(val, list):
-                self.__dict__[f'_{i}'] = CustomList(val)
+                _list[i] = CustomList(val)
             elif isinstance(val, dict):
-                self.__dict__[f'_{i}'] = CustomDict(val)
-            else:
-                self.__dict__[f'_{i}'] = val
+                _list[i] = CustomDict(val)
+
+        list.__init__(self, _list)
