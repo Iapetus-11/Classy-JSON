1c9ca66b5bee8d3359e6c1e71ba6bff0aca367bc
Iapetus11 <olimdahacker@gmail.com>
2020-08-19 19:26:08 -0400

    Merge CustomList and CustomDict to avoid circular import errors

--- classy_json/CustomList.py
+++
@@ -1,9 +0,0 @@
-class CustomList(list):
-    def __init__(self, _list):
-        for i, val in enumerate(_list):
-            if isinstance(val, list):
-                _list[i] = CustomList(val)
-            elif isinstance(val, dict):
-                _list[i] = CustomDict(val)
-
-        list.__init__(self, _list)
--- classy_json/CustomTypes.py
+++ classy_json/CustomTypes.py
@@ -1,3 +1,13 @@
+class CustomList(list):
+    def __init__(self, _list):
+        for i, val in enumerate(_list):
+            if isinstance(val, list):
+                _list[i] = CustomList(val)
+            elif isinstance(val, dict):
+                _list[i] = CustomDict(val)
+
+        list.__init__(self, _list)
+
 class CustomDict(dict):
     def __init__(self, _dict):
         dict.__init__(self, _dict)
