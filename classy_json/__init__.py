2b484294c3548245389dd1c2690ca5c6558a80ab
Iapetus11 <olimdahacker@gmail.com>
2020-08-19 18:13:51 -0400

    add loads() and load() for json module compatibility

--- classy_json/__init__.py
+++ classy_json/__init__.py
@@ -1,2 +1,16 @@
 from .CustomList import CustomList
 from .CustomDict import CustomDict
+import json
+
+def loads(data: str):
+    _data = json.loads(data)
+
+    if isinstance(_data, list):
+        return CustomList(_data)
+    elif isinstance(_data, dict):
+        return CustomDict(_data)
+    else:
+        return data
+
+def load(io):
+    return loads(io.read())
