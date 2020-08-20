046d3f4a6e876c13e50ab24a9cc85e16772ba834
Iapetus-11 <olimdahacker@gmail.com>
2020-08-20 19:15:51 -0400

    remove extra commas from CustomList

--- classyjson/CustomTypes.py
+++ classyjson/CustomTypes.py
@@ -3,9 +3,9 @@ class CustomList(list):
     def __init__(self, _list):
         for i, val in enumerate(_list):
             if isinstance(val, list):
-                _list[i] = CustomList(val,)
+                _list[i] = CustomList(val)
             elif isinstance(val, dict):
-                _list[i] = CustomDict(val,)
+                _list[i] = CustomDict(val)
 
         list.__init__(self, _list)
 
