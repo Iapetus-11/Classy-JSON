da4800078266eee2d94ffbcbec5b1d87f8eca29b
Iapetus-11 <olimdahacker@gmail.com>
2020-08-20 19:30:46 -0400

    add missing comma in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Iapetus-11/classy-json',
-    ext_modules = cythonize('classyjson/CustomTypes.pyx')
+    ext_modules = cythonize('classyjson/CustomTypes.pyx'),
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
