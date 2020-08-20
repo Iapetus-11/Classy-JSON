bbcf71055988c0d0d0d2e9cfb202529f842f48a3
Iapetus-11 <olimdahacker@gmail.com>
2020-08-19 21:49:12 -0400

    make setup.py more conventional looking

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import setuptools
 
-
-with open('README.md', 'r') as readme_file:
-    long_desc = readme_file.read()
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='Classy-Json',
     version='1.0.0',
     author='Iapetus-11',
     description='An attempt to recreate the way json behaves in Javascript, but in Python',
-    long_description=long_desc,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Iapetus-11/Classy-Json',
     classifiers=[
