7552adc32d5603faf48b0ee744374d139a181467
Iapetus-11 <olimdahacker@gmail.com>
2020-08-20 22:12:44 -0400

    increased granularity for second time test

--- tests/time_test.py
+++ tests/time_test.py
@@ -67,5 +67,5 @@ jj_avg = sum(jj_times) / len(jj_times)
 cj_avg = sum(cj_times) / len(cj_times)
 
 print('\n\n setting an attribute / key to a value:')
-print('        json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(jj_avg, 4), round(jj_total, 4), iterations))
-print(' classy-json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(cj_avg, 4), round(cj_total, 4), iterations))
+print('        json average time: {:2.6f} seconds || total time: {} || total iterations: {}'.format(round(jj_avg, 6), round(jj_total, 4), iterations))
+print(' classy-json average time: {:2.6f} seconds || total time: {} || total iterations: {}'.format(round(cj_avg, 6), round(cj_total, 4), iterations))
