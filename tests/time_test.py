ec63bbc831a7dd38ca570c6dcad8d74be38bf595
Iapetus-11 <olimdahacker@gmail.com>
2020-08-20 21:28:04 -0400

    make iterations for second time test 10000

--- tests/time_test.py
+++ tests/time_test.py
@@ -37,6 +37,8 @@ print('\n Loading json:')
 print('        json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(jj_avg, 4), round(jj_total, 4), iterations))
 print(' classy-json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(cj_avg, 4), round(cj_total, 4), iterations))
 
+iterations = 10000
+
 # Using regular json
 print(f'classy-json test using regular json ({iterations} iterations)')
 jj_times = []
