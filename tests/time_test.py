e4538586e680f101cbde8c3eedf18ced8eb8e886
Iapetus-11 <olimdahacker@gmail.com>
2020-08-20 21:25:07 -0400

    add more tests

--- tests/time_test.py
+++ tests/time_test.py
@@ -14,27 +14,56 @@ with open(file, 'r') as f:
 print(f'classy-json test using regular json ({iterations} iterations)')
 jj_times = []
 jj_total_start = perf_counter()
-for i in range(iterations):
+for _ in range(iterations):
     start = perf_counter()
     json.loads(raw)
     jj_times.append(perf_counter() - start)
-    print(f'iteration: {i+1} time: {jj_times[i]}')
 jj_total = perf_counter() - jj_total_start
 
 # Using classyjson
 print(f'classy-json test using classy-json ({iterations} iterations)')
 cj_times = []
 cj_total_start = perf_counter()
-for i in range(iterations):
+for _ in range(iterations):
     start = perf_counter()
     classyjson.loads(raw)
     cj_times.append(perf_counter() - start)
-    print(f'iteration: {i+1} time: {cj_times[i]}')
 cj_total = perf_counter() - cj_total_start
 
 jj_avg = sum(jj_times) / len(jj_times)
 cj_avg = sum(cj_times) / len(cj_times)
 
-print()
+print('\n Loading json:')
+print('        json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(jj_avg, 4), round(jj_total, 4), iterations))
+print(' classy-json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(cj_avg, 4), round(cj_total, 4), iterations))
+
+# Using regular json
+print(f'classy-json test using regular json ({iterations} iterations)')
+jj_times = []
+jj = json.loads(raw)
+jj_total_start = perf_counter()
+for _ in range(iterations):
+    start = perf_counter()
+    jj['a'] = 'a'
+    jj['a'] = 'a'
+    jj_times.append(perf_counter() - start)
+jj_total = perf_counter() - jj_total_start
+
+# Using classyjson
+print(f'classy-json test using classy-json ({iterations} iterations)')
+cj_times = []
+cj = classyjson.loads(raw)
+cj_total_start = perf_counter()
+for _ in range(iterations):
+    start = perf_counter()
+    cj['a'] = 'a'
+    cj.a = 'a'
+    cj_times.append(perf_counter() - start)
+cj_total = perf_counter() - cj_total_start
+
+jj_avg = sum(jj_times) / len(jj_times)
+cj_avg = sum(cj_times) / len(cj_times)
+
+print('\n\n setting an attribute / key to a value:')
 print('        json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(jj_avg, 4), round(jj_total, 4), iterations))
 print(' classy-json average time: {:2.4f} seconds || total time: {} || total iterations: {}'.format(round(cj_avg, 4), round(cj_total, 4), iterations))
