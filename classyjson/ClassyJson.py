bc448fef6ee77f47318b050ac920370e6a5656a7
Iapetus-11 <olimdahacker@gmail.com>
2020-09-09 09:14:24 -0400

    Change names of some vars and classes to fit the name of the package

--- classyjson/ClassyJson.py
+++ classyjson/ClassyJson.py
@@ -1,12 +1,12 @@
-from .CustomTypes import NiceList, NiceDict
+from .CustomTypes import NiceList, ClassyDict
 from json import *
 import json
 
-def objectify(data):
+def _classify(data):
     if isinstance(data, list):
         return NiceList(data)
     elif isinstance(data, dict):
-        return NiceDict(data)
+        return ClassyDict(data)
     else:
         return data
 
@@ -16,10 +16,10 @@ def loads(data, cls=None, object_hook=None, parse_float=None, parse_int=None, pa
         parse_constant=parse_constant, object_pairs_hook=object_pairs_hook
     )
 
-    return objectify(_data,)
+    return _classify(_data,)
 
 def load(io, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
     _data = json.load(io, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
     parse_constant=parse_constant, object_pairs_hook=object_pairs_hook)
 
-    return objectify(_data)
+    return _classify(_data)
--- classyjson/CustomTypes.py
+++ classyjson/CustomTypes.py
@@ -1,25 +1,25 @@
 
-def nice(val):
+def classify(val):
     if isinstance(val, list):
         return NiceList(val)
 
     if isinstance(val, dict):
-        return NiceDict(val)
+        return ClassyDict(val)
 
     return val
 
 
 class NiceList(list):
-    def __init__(self, _list: list = []):
-        list.__init__(self, [*map(nice, _list)])
+    def __init__(self, _list):
+        list.__init__(self, map(classify, _list))
 
 
-class NiceDict(dict):
-    def __init__(self, _dict: dict = {}):
-        dict.__init__(self, {k:nice(v) for (k, v) in _dict.items()})
+class ClassyDict(dict):
+    def __init__(self, _dict):
+        dict.__init__(self, {k:classify(v) for (k, v) in _dict.items()})
 
     __getattr__ = dict.__getitem__
     __delattr__ = dict.__delitem__
 
-    def __setattr__(self, name, value):  # NiceDict.a = 'something'
-        return dict.__setitem__(self, name, nice(value))
+    def __setattr__(self, name, value):  # ClassyDict.a = 'something'
+        return dict.__setitem__(self, name, classify(value))
