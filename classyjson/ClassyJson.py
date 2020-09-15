f56d4216019894e9c7b7dce6b14ac4bc2cb3c394
Iapetus-11 <olimdahacker@gmail.com>
2020-09-15 12:22:16 -0400

    expose classify() function and remove extra code

--- classyjson/ClassyJson.py
+++ classyjson/ClassyJson.py
@@ -1,14 +1,6 @@
-from .CustomTypes import NiceList, ClassyDict
+from .CustomTypes import *
 from json import *
 import json
-
-def _classify(data):
-    if isinstance(data, list):
-        return NiceList(data)
-    elif isinstance(data, dict):
-        return ClassyDict(data)
-    else:
-        return data
 
 def loads(data, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
     _data = json.loads(
@@ -16,10 +8,10 @@ def loads(data, cls=None, object_hook=None, parse_float=None, parse_int=None, pa
         parse_constant=parse_constant, object_pairs_hook=object_pairs_hook
     )
 
-    return _classify(_data,)
+    return classify(_data,)
 
 def load(io, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
     _data = json.load(io, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
     parse_constant=parse_constant, object_pairs_hook=object_pairs_hook)
 
-    return _classify(_data)
+    return classify(_data)
