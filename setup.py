641a464441658b3e8d16540e966de76fe50d3bea
Iapetus-11 <olimdahacker@gmail.com>
2020-09-15 12:22:23 -0400

    2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='classy-json',
-    version='2.0.0',
+    version='2.0.1',
     author='Iapetus-11',
     description='An attempt to recreate the way json behaves in Javascript, but in Python',
     long_description=long_description,
