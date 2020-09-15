6421800c6df61e948c87d3824f903e94d4ec52df
Iapetus-11 <olimdahacker@gmail.com>
2020-09-15 12:24:01 -0400

    remove extra comma

--- classyjson/ClassyJson.py
+++ classyjson/ClassyJson.py
@@ -8,7 +8,7 @@ def loads(data, cls=None, object_hook=None, parse_float=None, parse_int=None, pa
         parse_constant=parse_constant, object_pairs_hook=object_pairs_hook
     )
 
-    return classify(_data,)
+    return classify(_data)
 
 def load(io, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
     _data = json.load(io, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
