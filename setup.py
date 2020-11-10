b53cab060eca4da177c0bd5a4f2138ae2946625d
Iapetus-11 <olimdahacker@gmail.com>
2020-11-10 10:56:29 -0500

    consistency changes

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setuptools.setup(
     author='Iapetus-11',
     description='An attempt to recreate the way json behaves in Javascript, but in Python',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/Iapetus-11/classy-json',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6'
 )
