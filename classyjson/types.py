1aca92363402eacd08a7de6c64ff518adbbcd44b
Iapetus-11 <olimdahacker@gmail.com>
2020-11-20 16:20:19 -0500

    comments

--- classyjson/types.py
+++ classyjson/types.py
@@ -9,7 +9,7 @@ def classify(thing):
     if isinstance(thing, dict):
         return ClassyDict(thing)
 
-    if isinstance(thing, (thing, tuple)):
+    if isinstance(thing, (list, tuple)):  # this adds support for iterables which are subclasses or instances of lists and tuples
         return type(thing)(classify(item) for item in thing)
 
     return thing
@@ -19,16 +19,17 @@ class ClassyDict(dict):
     """dict subclass required for dot access"""
 
     def __init__(self, _dict=None):
-        if _dict is None:
+        if _dict is None:  # allow for creating a new ClassyDict via CLassyDict()
             _dict = {}
 
         dict.__init__(self, {k:classify(v) for (k, v) in _dict.items()})
 
+    # Override the attribute methods to add dot access
     __getattr__ = dict.__getitem__
     __delattr__ = dict.__delitem__
 
-    def __setattr__(self, name, value):  # ClassyDict.a = 'something'
+    def __setattr__(self, name, value):  # Add dot-access ClassyDict.a = 'something'
         return dict.__setitem__(self, name, classify(value))
 
-    def copy(self):
+    def copy(self):  # this actually is a deep copy
         return classify(dict.copy(self))
