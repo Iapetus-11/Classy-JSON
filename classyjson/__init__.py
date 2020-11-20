511156fcc2db444b270a81fc8545a12e0c164bfc
Iapetus-11 <olimdahacker@gmail.com>
2020-11-20 15:20:11 -0500

    more specific imports

--- classyjson/__init__.py
+++ classyjson/__init__.py
@@ -8,7 +8,7 @@ from json import loads as json_loads
 from json import load as json_load
 from json import *
 
-from .types import *
+from .types import classify, ClassyDict
 
 def loads(*args, **kwargs):
     """Override loads function"""
