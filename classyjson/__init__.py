77d1d2a2525d27144a5115987ded534bdbd96cd5
Iapetus-11 <olimdahacker@gmail.com>
2020-11-20 14:56:06 -0500

    major restructuring

--- classyjson/__init__.py
+++ classyjson/__init__.py
@@ -2,7 +2,22 @@
 Dot-access for Python dictionaries and more
 """
 
-from .ClassyJson import *
-from .Types import *
+from json import loads as json_loads
+from json import load as json_load
+from json import *
 
-__version__ = '2.3.0'
+from .types import *
+
+def loads(data, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
+    _data = json_loads(
+        data, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
+        parse_constant=parse_constant, object_pairs_hook=object_pairs_hook
+    )
+
+    return classify(_data)
+
+def load(io, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None):
+    _data = json_load(io, cls=cls, object_hook=object_hook, parse_float=parse_float, parse_int=parse_int,
+    parse_constant=parse_constant, object_pairs_hook=object_pairs_hook)
+
+    return classify(_data)
