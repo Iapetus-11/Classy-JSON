35051c179c8d1e984a571d9c589f144e98d1dc58
Milo Weinberg <iapetus011@gmail.com>
2021-04-22 11:18:20 -0400

    attempt to fix setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 import setuptools  # import order matters here wth
 
-setuptools.dist.Distribution().fetch_build_eggs(["Cython>=0.15.1"])
-
-from Cython.Build import cythonize
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="classy-json-cython",
-    version="1.0.3",
+    version="1.0.4",
     author="Iapetus-11",
     description="Dot access for Python dictionaries",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Iapetus-11/classy-json",
+    setup_requires=["cython"],
     packages=setuptools.find_packages(),
-    ext_modules=cythonize("classyjson/*.pyx"),
+    ext_modules=[
+        setuptools.Extension('classyjson.types', sources=['classyjson/types.pyx'])
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.6"
 )
