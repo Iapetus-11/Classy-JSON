70c4f556f21c5b533c0745bd3f7ea39d28b1f97c
Milo Weinberg <iapetus011@gmail.com>
2021-04-22 10:53:09 -0400

    change package name

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="classy-json",
-    version="4.0.0",
+    name="classy-json-cython",
+    version="1.0.0",
     author="Iapetus-11",
     description="Dot access for Python dictionaries",
     long_description=long_description,
